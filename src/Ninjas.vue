69d1895019d610328abd2d40407a87920c0286d9
0racularGh0st <nigel4tariang@gmail.com>
2020-09-12 21:43:33 +0530

    Components

---
+++ src/Ninjas.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <li v-for="titan in titans">{{titan}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      titans: ['Eren','Mikasa','Armin']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import Ninjas from './Ninjas.vue';
+// Vue.component('ninjas',Ninjas); //Globally register a component
 
 new Vue({
   el: '#app',
